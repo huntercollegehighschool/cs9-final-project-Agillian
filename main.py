d1a5d8804eb756188994f86740903b110bff269e
Agillian <103528665+Agillian@users.noreply.github.com>
2022-06-12 00:59:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,57 @@
 """
-Name(s):
-Name of Project:
+Name(s): Ayden and Justin
+Name of Project: It's Time to Play HANGMAN!
 """
+import time
+import random
+import page1
+import page2
+from page1 import stages
+from page2 import list
 
-#Write the main part of your program here. Use of the other pages is optional.
+print("H-A-N-G-M-A-N: By Justin Lin and Ayden Gillian")
+time.sleep(1)
+hi = input("Do you want to play Hangman? ")
+yes = "yes"
+Yes = "Yes"
+YES = "YES"
+if hi == yes or Yes or YES:
+  print("Let's start!")
+elif hi != yes or Yes or YES:
+  print("Awww, maybe next time!")
 
-#import page1  # uncomment if you're using page1
-#import page2  # uncomment if you're using page2
-#import page3  # uncomment if you're using page3
-#import page4  # uncomment if you're using page4
+hang_word = random.choice(list)
+hang_length = len(hang_word)
+
+lost_game = False
+numlives = 7
+
+print("_ " * hang_length)
+HANGMAN = []
+for _ in range(hang_length):
+  HANGMAN += "_"
+while not lost_game:
+  guess = input("Enter a letter: ").lower()
+  if guess in HANGMAN:
+    print(f"You have already guess '{guess}'")
+    guess = input("Guess a letter:").lower()
+  for position in range(hang_length):
+    letter = hang_word[position]
+    if letter == guess:
+      HANGMAN[position] = letter 
+      print("You've got a letter!")
+      
+  if guess not in hang_word:
+    print(f"The letter {guess} is not in the word :(")
+    numlives -= 1
+    if numlives == 0:
+        lost_game = True
+        print("Aw man...you lose")
+        print(f"The word was: {hang_word}")
+
+  print(f"{''.join(HANGMAN)}")
+  if "_" not in HANGMAN:
+      lost_game = True
+      print("CONGRATS! You've beat the game!")
+
+  print(stages[numlives])
